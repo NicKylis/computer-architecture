faf39e66f6c16459454331feee8d96ac56a1fd86
ExecuteExe <117383317+ExecuteExe1@users.noreply.github.com>
2024-11-09 16:02:24 +0200

    Add files via upload

---
+++ codes/Code1.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+#define lm 6
+#define ln 6
+#define lp 6
+
+#define m (1 << lm)
+#define n (1 << ln)
+#define p (1 << lp)
+void MATRIX_MUL(int A[m][n], int B[n][p], int C[m][p]);
+void software_matrix_mul(int A[m][n], int B[n][p], int C[m][p]) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            int sum = 0;
+            for (int k = 0; k < n; k++) {
+                sum += A[i][k] * B[k][j];
+            }
+            C[i][j] = sum;
+        }
+    }
+}
+int main(){
+ int comp=0;
+    int A[m][n];
+    int B[n][p];
+    int C[m][p];
+    int D[m][p];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            A[i][j] = (rand() % 256);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < p; j++){
+            B[i][j] =  (rand() % 256);
+        }
+    }
+   software_matrix_mul(A,B,D);
+
+    MATRIX_MUL(A, B, C);
+
+    for (int i = 0; i < m; i++) {
+           for (int j = 0; j < n; j++) {
+        	    if(C[i][j]!=D[i][j]){
+        	    	comp+=comp+1;
+        	    }
+        	    else{
+        	    	break;
+        	    }
+           }
+    }
+     if(comp==0){
+    	 printf("Test Passed! ");
+     }
+     else{
+    	 printf("Test failed!");
+     }
+    return 0;
+}
---
+++ codes/tbCode1.cpp
@@ -0,0 +1,36 @@
+#include <hls_stream.h>
+
+//estw exoyme autes tis dunameis tou 2
+#define lm 2
+#define ln 3
+#define lp 4
+//shift aristera gia na paroume to dim twn pinakwn afou einai vasismenoi se 2^2 gt p.x to 2^2=4  ara to lm=2 =0010 ena <shift 0100=4
+#define m (1 << lm)
+#define n (1 << ln)
+#define p (1 << lp)
+
+void MATRIX_MUL(int A[m][n], int B[n][p], int C[m][p]) {
+    // HLS PRAGMA directives gia optimization
+    #pragma HLS ARRAY_PARTITION variable=A complete dim=2
+    #pragma HLS ARRAY_PARTITION variable=B complete dim=1
+    #pragma HLS ARRAY_PARTITION variable=C complete dim=2
+
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            #pragma HLS PIPELINE
+            C[i][j] = 0;
+        }
+    }
+
+#pragma HLS UNROLL
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            for (int k = 0; k < n; k++){
+            	//reduces loop overhead
+#pragma HLS PIPELINE
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
